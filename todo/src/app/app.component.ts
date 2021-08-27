27f810d9f9f3157c94b74d2475afc83a7f306b89
PH Assis <pieit@live.com>
2021-08-27 13:36:00 -0300

    Created my first component

---
+++ todo/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from "@angular/core";
+import { Todo } from "../models/todo.model";
+
+@Component({
+	selector: "app-root",
+	templateUrl: "./app.component.html",
+	styleUrls: ["./app.component.scss"]
+})
+export class AppComponent {
+	public todos: Todo[] = [];
+	public title: String = "Minhas Tarefas";
+
+	constructor() {
+		this.todos.push(new Todo(1, "Passear com o cachorro", false));
+		this.todos.push(new Todo(2, "Ir ao supermercado", false));
+		this.todos.push(new Todo(3, "Cortar o cabelo", false));
+	}
+
+	alteraTexto() {
+		this.title = "Teste";
+	}
+}
